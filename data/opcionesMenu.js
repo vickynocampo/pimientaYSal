edce39481d567cf0d615b6bcaa644e98a5c18118
vickynocampoemail <you@example.com>
2022-06-23 16:49:26 -0300

    imagenes dinamicas

--- data/opcionesMenu.js
+++ data/opcionesMenu.js
@@ -3,35 +3,48 @@ const opcionesMenu = [
         id: 1,
         nombre: "Carpaccio fresco",
         descripcion: "Entrada Carpaccio de salmón con cítricos",
-        precio:  "U$S 65.50"
+        precio:  "U$S 65.50",
+        img:"/Carpaccio-de-salmon.jpg"
     }
     ,
     { 
         id: 2,
         nombre: "Risotto de berenjena",
         descripcion: "Risotto de berenjena y queso de cabra",
-        precio: "U$S 47.00"
+        precio: "U$S 47.00",
+        img: "/Risotto-berenjena-queso-cabra.jpg"
     }
     ,
     {
         id: 3,    
         nombre: "Mousse de arroz",
         descripcion: "Mousse de arroz con leche y aroma de azahar",
-        precio: " U$S 27.50"
+        precio: " U$S 27.50",
+        img: "/Mousse-de-arroz-con-leche.jpg"   
     }
     ,
     {
         id: 4,
         nombre: "Espárragos blancos",
         descripcion: "Espárragos blancos con vinagreta de verduras y jamón ibérico",
-        precio: " U$S 37.50"
+        precio: " U$S 37.50",
+        img: "/esparragos.png"  
     }
     ,
     {
         id: 5,
         nombre: "Papas Fritas",
         descripcion: "Papas rusticas con hierbas",
-        precio: " U$S 22.50"
+        precio: " U$S 22.50",
+
+    }
+    ,
+    {
+        id: 6,
+        nombre: "Pizza",
+        descripcion: "Pizza a la piedra",
+        precio: " U$S 10.50",
+    
     }
 ];
 
